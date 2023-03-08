4df50ef3654cfefca02094c09b7b572574ff7688
Sergii Buchynskyi <Buchik_zt@hotmail.com>
2023-03-08 21:38:23 +0200

    remote-driver IP update

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -64,7 +64,7 @@ public class Driver {
                     break;
                 case "remote-chrome":
                     // assign your grid server address
-                    String gridAdress = "3.83.36.248/"; // put your own Linux grid IP here
+                    String gridAdress = "3.83.36.248"; // put your own Linux grid IP here
                     try {
                         URL url = new URL("http://"+gridAdress+":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
